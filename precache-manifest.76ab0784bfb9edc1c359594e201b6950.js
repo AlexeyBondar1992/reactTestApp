46bba964d818d94d84130577fdafecffbeba0b11
a4.bondar <a4.bondar@itransition.com>
2019-05-29 17:20:15 +0300

    built app

---
+++ precache-manifest.76ab0784bfb9edc1c359594e201b6950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227e655f03a6ffc6de7776a2c87aa4fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a1d45b88ab525a9ea5e",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9ef34314f6f70305a4f7",
+    "url": "/static/css/main.b7722436.chunk.css"
+  },
+  {
+    "revision": "3a1d45b88ab525a9ea5e",
+    "url": "/static/js/2.3cb77fba.chunk.js"
+  },
+  {
+    "revision": "9ef34314f6f70305a4f7",
+    "url": "/static/js/main.25c278ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
